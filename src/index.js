8cbf41fa9016a9a437c8dc57a2f5ab25f4586148
helenanic <helenanicolodelli@gmail.com>
2017-07-07 15:23:51 -0300

    Commit

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
  import React, { Component } from 'react';
  import ReactDOM from 'react-dom';
- import SearchBar from './components/search_bar';
  import YTSearch from 'youtube-api-search';
-
-const API_KEY = 'AIzaSyDHAu4TSVyf5OCnsSAAG4QzmPwHi1g0rUQ'; 
+ import SearchBar from './components/search_bar';
+ import VideoList from './components/video_list';
+ import VideoDetail from './components/video_detail';
+ const API_KEY = 'AIzaSyDHAu4TSVyf5OCnsSAAG4QzmPwHi1g0rUQ'; 
 
 class App extends Component {
     constructor(props){
         super(props);
 
-        this.state = { videos: [] };
+        this.state = { 
+            videos: [],
+            selectedVideo: null
+        };
 
-        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-            this.setState({ videos });
+        YTSearch({key: API_KEY, term: 'sugar glider'}, (videos) => {
+            this.setState({ 
+                videos: videos,
+                selectedVideo: videos[0]
+            });
         });
     }
 
@@ -20,6 +27,10 @@ class App extends Component {
         return (
             <div>
                 <SearchBar />
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList 
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                    videos={this.state.videos} />
             </div>
         );
     }
